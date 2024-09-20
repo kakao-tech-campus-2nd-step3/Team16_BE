25c6747e39203b410632a9c8383173a493308f89
박민규 <73173356+ps9319@users.noreply.github.com>
2024-09-20 22:56:21 +0900

    Week3 entity 구현 (#5)
    * chore: git ignore 추가
    
    * feat: set up the project
    
    * feat(Member): 회원 entity 생성
    
    * feat(Food, FoodCategory, MeetingNonPreferenceFood, MeetingPreferenceFood, NonPreferenceFood, PreferenceFood): entity 생성
    
    * refactor(Food, FoodCategory, MeetingNonPreferenceFood, MeetingPreferenceFood, NonPreferenceFood, PreferenceFood): id 수정
    
    * refactor(Food, FoodCategory, MeetingNonPreferenceFood, MeetingPreferenceFood, NonPreferenceFood, PreferenceFood): 타입 수정
    
    * refactor(Food, FoodCategory, MeetingNonPreferenceFood, MeetingPreferenceFood, NonPreferenceFood, PreferenceFood): 소문자로 수정
    
    * feat(Event, PersonalCalendar, Reminder, Time): 일정 entity, 개인 캘린더 entity, 리마인더 entity, 시간 entity 생성 (#3)
    
    * feat(Event): 일정 entity 생성
    
    * feat(PersonalCalendar): 개인 캘린더 entity 생성
    
    * feat(Reminder): 리마인더 entity 생성
    
    * feat(Time): 시간 entity 생성
    
    * refactor(Event): eventId 타입 변경
    
    * refactor(Time): startAt, endAt 타입 변경
    
    * feat(Location, Meeting, MeetingEvent, MemberMeeting): 위치, 모임, 모임 일정, … (#4)
    
    * feat(Location, Meeting, MeetingEvent, MemberMeeting): 위치, 모임, 모임 일정, 모임 참가자 entity 생성
    
    * feat(Meeting): JoinColumn 어노테이션 추가
    
    * feat(MeetingEvent): 연관관계 추가
    
    * feat(MeetingEvent): JoinColumn 어노테이션 추가
    
    ---------
    
    Co-authored-by: Yeonwoo JI <95765163+speciling@users.noreply.github.com>
    Co-authored-by: 서지우 <142224090+seojiu@users.noreply.github.com>
    Co-authored-by: seojiu <seojiu5598@gmail.com>
    Co-authored-by: Haruin <138747411+KBM05haruin30@users.noreply.github.com>

---
+++ src/main/java/org/cookieandkakao/babting/domain/calendar/entity/Time.java
@@ -0,0 +1,31 @@
+package org.cookieandkakao.babting.domain.calendar.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "time")
+public class Time {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long timeId;
+
+    @Column(nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(nullable = false)
+    private LocalDateTime endAt;
+
+    @Column(nullable = false)
+    private String timeZone;
+
+    @Column(nullable = false)
+    private boolean allDay;
+
+}
