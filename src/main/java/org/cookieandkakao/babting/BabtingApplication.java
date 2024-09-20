822103baf7f5dc5e026101ee1e443251943490a1
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-09-20 14:35:36 +0900

    feat: set up the project

---
+++ src/main/java/org/cookieandkakao/babting/BabtingApplication.java
@@ -0,0 +1,13 @@
+package org.cookieandkakao.babting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabtingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabtingApplication.class, args);
+    }
+
+}
