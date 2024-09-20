9e953577e7e2ab6c55f2ea4517b7be0fdd891318
박민규 <pmq0101@naver.com>
2024-09-20 21:22:24 +0900

    feat(MeetingEvent): 연관관계 추가

--- src/main/java/org/cookieandkakao/babting/domain/meeting/entity/MeetingEvent.java
+++ src/main/java/org/cookieandkakao/babting/domain/meeting/entity/MeetingEvent.java
@@ -3,6 +3,8 @@ package org.cookieandkakao.babting.domain.meeting.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import org.cookieandkakao.babting.domain.calendar.entity.Time;
 
@@ -11,6 +13,10 @@ public class MeetingEvent {
     @Id
     @GeneratedValue
     private Long meetingEventId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_meeting_id")
+    private MemberMeeting memberMeeting;
 
     @OneToOne
     private Time time;
