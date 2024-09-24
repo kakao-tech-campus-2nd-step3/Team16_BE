61140e4fd54bcb2d61dba84c543102fa3bf1e943
박민규 <73173356+ps9319@users.noreply.github.com>
2024-09-24 19:22:03 +0900

    week3 리뷰 반영 (#8)
    * refactor(Location, Meeting, MemberMeeting): 코드 정리
    
    * feat(Location, Meeting, MeetingEvent, MemberMeeting): Table 어노테이션 추가
    
    * feat(Location): nullable 추가

--- src/main/java/org/cookieandkakao/babting/domain/meeting/entity/Meeting.java
+++ src/main/java/org/cookieandkakao/babting/domain/meeting/entity/Meeting.java
@@ -7,33 +7,44 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Entity
+@Table(name = "meeting")
 public class Meeting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long meetingId;
+
     @OneToOne
     @JoinColumn(name = "base_location_id")
     private Location baseLocation;
+
     @OneToOne
     @JoinColumn(name = "meeting_location_id")
     private Location meetingLocation;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private LocalDate startDate;
+
     @Column(nullable = false)
     private LocalDate endDate;
+
     @Column(nullable = false)
     private Integer durationTime;
+
     @Column(nullable = false)
     private LocalTime startTime;
+
     @Column(nullable = false)
     private LocalTime endTime;
+
     private LocalDateTime confirmDateTime;
 }
