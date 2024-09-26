c4d83ad9d7b8ddf19310bec4513d76be2a3e506e
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-09-26 21:22:51 +0900

    refactor(TalkCalendarController): RequestParam -> RequestBody로 수정 쿼리 파라미터가 아닌 JSON 형식으로 받을 수 있도록 수정했습니다.

--- src/main/java/org/cookieandkakao/babting/domain/calendar/controller/TalkCalendarController.java
+++ src/main/java/org/cookieandkakao/babting/domain/calendar/controller/TalkCalendarController.java
@@ -2,13 +2,14 @@ package org.cookieandkakao.babting.domain.calendar.controller;
 
 import java.util.Collections;
 import org.cookieandkakao.babting.domain.calendar.dto.EventListDTO;
+import org.cookieandkakao.babting.domain.calendar.dto.EventListRequestDTO;
 import org.cookieandkakao.babting.domain.calendar.service.TalkCalendarService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,11 +25,13 @@ public class TalkCalendarController {
     @GetMapping("/events")
     public ResponseEntity<EventListDTO> getEventList(
         @RequestHeader(value = "Authorization") String authorizationHeader,
-        @RequestParam(value = "from") String from,
-        @RequestParam(value = "to") String to
+        @RequestBody EventListRequestDTO eventListRequestDTO
     ) {
         String accessToken = authorizationHeader.replace("Bearer ", "");
         try {
+            String from = eventListRequestDTO.from();
+            String to = eventListRequestDTO.to();
+
             EventListDTO eventList = talkCalendarService.getEventList(accessToken, from, to);
 
             HttpStatus status = HttpStatus.OK;
