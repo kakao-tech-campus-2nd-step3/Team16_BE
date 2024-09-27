da8b482126a82143319cc69e9d056d58b76d5938
speciling <speciling@naver.com>
2024-09-27 14:00:23 +0900

    feat(AuthController, AuthService, KakaoOAuthTokenDto): 카카오 OAuth 토큰 요청 기능 구현

--- src/main/java/org/cookieandkakao/babting/domain/member/controller/AuthController.java
+++ src/main/java/org/cookieandkakao/babting/domain/member/controller/AuthController.java
@@ -1,8 +1,10 @@
 package org.cookieandkakao.babting.domain.member.controller;
 
+import org.cookieandkakao.babting.domain.member.dto.KakaoOAuthTokenDto;
 import org.cookieandkakao.babting.domain.member.service.AuthService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,5 +21,13 @@ public class AuthController {
     @GetMapping("/login")
     public ModelAndView login() {
         return new ModelAndView("redirect:" + authService.getAuthUrl());
+    }
+
+    @GetMapping("/login/code/kakao")
+    public String getToken(@RequestParam(name = "code") String authorizeCode) {
+
+        KakaoOAuthTokenDto kakaoToken = authService.requestKakaoToken(authorizeCode);
+
+        return kakaoToken.getAccessToken();
     }
 }
--- src/main/java/org/cookieandkakao/babting/domain/member/service/AuthService.java
+++ src/main/java/org/cookieandkakao/babting/domain/member/service/AuthService.java
@@ -1,15 +1,23 @@
 package org.cookieandkakao.babting.domain.member.service;
 
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
+
+import org.cookieandkakao.babting.domain.member.dto.KakaoOAuthTokenDto;
 import org.cookieandkakao.babting.domain.member.properties.KakaoClientProperties;
 import org.cookieandkakao.babting.domain.member.properties.KakaoProviderProperties;
 import org.cookieandkakao.babting.domain.member.util.AuthorizationUriBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class AuthService {
 
     private final KakaoClientProperties kakaoClientProperties;
     private final KakaoProviderProperties kakaoProviderProperties;
+    private final RestClient restClient = RestClient.builder().build();
 
     public AuthService(KakaoClientProperties kakaoClientProperties,
         KakaoProviderProperties kakaoProviderProperties) {
@@ -22,5 +30,27 @@ public class AuthService {
             .clientProperties(kakaoClientProperties)
             .providerProperties(kakaoProviderProperties)
             .build();
+    }
+
+    public KakaoOAuthTokenDto requestKakaoToken(String authorizeCode) {
+
+        String tokenUri = kakaoProviderProperties.tokenUri();
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("code", authorizeCode);
+        body.add("redirect_uri", kakaoClientProperties.redirectUri());
+        body.add("client_id", kakaoClientProperties.clientId());
+        body.add("client_secret", kakaoClientProperties.clientSecret());
+
+        ResponseEntity<KakaoOAuthTokenDto> entity = restClient.post()
+            .uri(tokenUri)
+            .contentType(APPLICATION_FORM_URLENCODED)
+            .body(body)
+            .retrieve()
+            .toEntity(KakaoOAuthTokenDto.class);
+
+        return entity.getBody();
+
     }
 }
