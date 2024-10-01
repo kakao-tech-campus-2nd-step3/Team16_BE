62c1db0d6bd63ad34c4d0da9d21b02242b32c551
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-10-01 12:59:32 +0900

    refactor(GlobalExceptionHandler): return 부분 수정

--- src/main/java/org/cookieandkakao/babting/domain/calendar/controller/GlobalExceptionHandler.java
+++ src/main/java/org/cookieandkakao/babting/domain/calendar/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.cookieandkakao.babting.domain.calendar.controller;
 import org.cookieandkakao.babting.common.apiresponse.ApiResponseBody;
 import org.cookieandkakao.babting.common.apiresponse.ApiResponseBody.FailureBody;
+import org.cookieandkakao.babting.common.apiresponse.ApiResponseGenerator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,10 +13,7 @@ public class GlobalExceptionHandler {
     // 모든 Exception을 처리하는 핸들러
     @ExceptionHandler(Exception.class)
     public ResponseEntity<FailureBody> handleAllExceptions(Exception ex) {
-        return new ResponseEntity<>(
-            new ApiResponseBody.FailureBody(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), ex.getMessage()),
-            HttpStatus.INTERNAL_SERVER_ERROR
-        );
+        return ApiResponseGenerator.fail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
 }
