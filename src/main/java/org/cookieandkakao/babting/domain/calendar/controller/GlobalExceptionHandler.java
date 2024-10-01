5aab3e737f436594b0eba7e56d026cdab1751dd3
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-10-01 12:56:07 +0900

    feat(GlobalExceptionHandler): 실패한 응답의 Exception을 처리하는 핸들러 추가

---
+++ src/main/java/org/cookieandkakao/babting/domain/calendar/controller/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.cookieandkakao.babting.domain.calendar.controller;
+import org.cookieandkakao.babting.common.apiresponse.ApiResponseBody;
+import org.cookieandkakao.babting.common.apiresponse.ApiResponseBody.FailureBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 모든 Exception을 처리하는 핸들러
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<FailureBody> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>(
+            new ApiResponseBody.FailureBody(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), ex.getMessage()),
+            HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+}
