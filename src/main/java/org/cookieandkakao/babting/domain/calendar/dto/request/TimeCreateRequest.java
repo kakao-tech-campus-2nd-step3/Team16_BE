31f36afe527acd961b17aba6881fa1f720942042
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-10-04 19:48:22 +0900

    feat(EventCreateRequest, TimeCreateRequest): 검증 어노테이션 추가

--- src/main/java/org/cookieandkakao/babting/domain/calendar/dto/request/TimeCreateRequest.java
+++ src/main/java/org/cookieandkakao/babting/domain/calendar/dto/request/TimeCreateRequest.java
@@ -2,6 +2,7 @@ package org.cookieandkakao.babting.domain.calendar.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -9,8 +10,11 @@ import org.cookieandkakao.babting.domain.calendar.entity.Time;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record TimeCreateRequest(
+
+    @NotBlank(message = "시작 시간은 필수입니다.")
     String startAt,
 
+    @NotBlank(message = "종료 시간은 필수입니다.")
     String endAt,
 
     String timeZone,
