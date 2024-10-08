11fff394702816289ad56006ff8f52640745d091
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-10-08 23:37:25 +0900

    feat(MemberController, MemberService, MemberProfileGetResponse): 회원 본인 프로필 조회 기능 구현 (#31)

---
+++ src/main/java/org/cookieandkakao/babting/domain/member/service/MemberService.java
@@ -0,0 +1,29 @@
+package org.cookieandkakao.babting.domain.member.service;
+
+import org.cookieandkakao.babting.domain.member.dto.MemberProfileGetResponse;
+import org.cookieandkakao.babting.domain.member.entity.Member;
+import org.cookieandkakao.babting.domain.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public MemberProfileGetResponse getMemberProfile(Long memberId) {
+        Member member = findMember(memberId);
+        return new MemberProfileGetResponse(memberId, member.getNickname(), member.getThumbnailImageUrl(),
+            member.getProfileImageUrl());
+    }
+
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId)
+            .orElseThrow(() -> new NoSuchElementException("해당 사용자가 존재하지 않습니다."));
+    }
+}
