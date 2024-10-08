e0ff7de75a12bd6b913d4a8926b9e493e1d74805
박민규 <pmq0101@naver.com>
2024-10-08 13:24:23 +0900

    remove(Meeting): 코드 정리

--- src/main/java/org/cookieandkakao/babting/domain/meeting/entity/Meeting.java
+++ src/main/java/org/cookieandkakao/babting/domain/meeting/entity/Meeting.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
