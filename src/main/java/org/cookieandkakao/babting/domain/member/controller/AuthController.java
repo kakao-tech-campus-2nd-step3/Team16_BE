a908ef8a756c3bac25df9bee64d0830ebda07853
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-11-12 22:43:31 +0900

    [FIX] 로그인 결과 redirect url 수정 (#62)

--- src/main/java/org/cookieandkakao/babting/domain/member/controller/AuthController.java
+++ src/main/java/org/cookieandkakao/babting/domain/member/controller/AuthController.java
@@ -62,7 +62,7 @@ public class AuthController {
             kakaoTokenDto = authService.requestKakaoToken(authorizeCode);
             kakaoMemberInfoDto = authService.requestKakaoMemberInfo(kakaoTokenDto);
         } catch (Exception e) {
-            return "redirect:/login/fail";  // 프론트 페이지 구현 후 수정 예정
+            return "redirect:https://www.babting.com/fail";
         }
 
         Long memberId = memberService.saveMemberInfoAndKakaoToken(kakaoMemberInfoDto,
@@ -71,7 +71,7 @@ public class AuthController {
 
         response.addCookie(createRefreshTokenCookie(tokenDto));
 
-        return "redirect:/login/success";  // 프론트 페이지 구현 후 수정 예정
+        return "redirect:https://www.babting.com/success";
     }
 
     @ResponseBody
